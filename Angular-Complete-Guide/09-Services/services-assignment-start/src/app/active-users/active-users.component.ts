0731445394b7d91acfffa8d278907f72c63ba1c9
unknown <Te0gop.u@gmail.com>
2022-11-01 23:42:38 +0200

    adding services

--- Angular-Complete-Guide/09-Services/services-assignment-start/src/app/active-users/active-users.component.ts
+++ Angular-Complete-Guide/09-Services/services-assignment-start/src/app/active-users/active-users.component.ts
@@ -1,15 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-active-users',
   templateUrl: './active-users.component.html',
   styleUrls: ['./active-users.component.css']
 })
-export class ActiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToInactive = new EventEmitter<number>();
+export class ActiveUsersComponent implements OnInit {
+   users: string[];
+  // @Output() userSetToInactive = new EventEmitter<number>();
+
+  constructor(private userService: UsersService) {}
+
+  ngOnInit(): void {
+    this.users = this.userService.activeUsers;
+  }
 
   onSetToInactive(id: number) {
-    this.userSetToInactive.emit(id);
+    // this.userSetToInactive.emit(id);
+    this.userService.onSetToInactive(id);
   }
 }
--- Angular-Complete-Guide/09-Services/services-assignment-start/src/app/inactive-users/inactive-users.component.ts
+++ Angular-Complete-Guide/09-Services/services-assignment-start/src/app/inactive-users/inactive-users.component.ts
@@ -1,15 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-inactive-users',
   templateUrl: './inactive-users.component.html',
   styleUrls: ['./inactive-users.component.css']
 })
-export class InactiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToActive = new EventEmitter<number>();
+export class InactiveUsersComponent implements OnInit {
+  users: string[];
+  // @Output() userSetToActive = new EventEmitter<number>();
+
+  constructor(private userService: UsersService) {}
+
+  ngOnInit(): void {
+    this.users = this.userService.inactiveUsers;
+  }
 
   onSetToActive(id: number) {
-    this.userSetToActive.emit(id);
+    // this.userSetToActive.emit(id);
+    this.userService.onSetToActive(id);
   }
 }
